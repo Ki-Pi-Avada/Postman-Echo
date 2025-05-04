5635229f1a32ef7e09e3d0f98a0840d65ddb42f8
Kifo <i.g.a.n.garsia@gmail.com>
2025-05-04 20:44:03 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPostedData() {
+        given()
+                .baseUri("https://postman-echo.com/")
+                .contentType("text/plain; charset=utf-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+    @Test
+    void shouldFailWithWrongAssertion() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("test data"));
+    }
+}
